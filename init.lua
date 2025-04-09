c9c10687efdafaac9d9cccad49e3e59ed2d0c22d
JAVV <vinas.johnandrei@gmail.com>
2025-04-09 12:59:34 +0800

    modify lazy setup

--- init.lua
+++ init.lua
@@ -1,4 +1,46 @@
-require("acolyptos.core.autocmds")
-require("acolyptos.core.options")
-require("acolyptos.core.keymaps")
-require("acolyptos.core.lazy")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    lazyrepo,
+    lazypath
+  })
+  
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." }
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Load lazy.nvim setup and plugin
+require("lazy_setup")
+
+-- Load core configurations
+require("core.autocmds")
+require("core.keymaps")
+require("core.options")
+
+-- Load LSP configurations
+require("lsp")
+
+-- Load plugin configurations
+require("plugins.colorscheme")
+require("plugins.lsp_related")
+require("plugins.treesitter")
+require("plugins.cmp")
+require("plugins.tester")
+require("plugins.debugger")
+require("plugins.gitsigns")
+require("plugins.coding")
+require("plugins.ui")
+require("plugins.telescope")

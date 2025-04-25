f69b9c266138c21f16361539dd2dadd5f3f41d61
JAVV <vinas.johnandrei@gmail.com>
2025-04-25 13:49:28 +0800

    changed colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,1 +1,37 @@
-vim.cmd("colorscheme nord")
+local catpuccin = require("catppuccin")
+catpuccin.setup({
+  background = {
+    light = "latte",
+    dark = "mocha"
+  },
+  transparent_background = false,
+  integrations = {
+    cmp = true,
+    gitsigns = true,
+    nvimtree = true,
+    treesitter = true,
+    notify = true,
+    mini = {
+      enabled = true,
+      indentscope_color = "",
+    },
+    alpha = true,
+    dap = true,
+    dap_ui = true,
+    native_lsp = {
+      enabled = true,
+      underlines = {
+        errors = { "underline" },
+        hints = { "underline" },
+        warnings = { "underline" },
+        information = { "underline" },
+        ok = { "underline" },
+      }
+    },
+    nvim_surround = true,
+    telescope = { enabled = true },
+
+  },
+})
+
+vim.cmd("colorscheme catppuccin-latte")

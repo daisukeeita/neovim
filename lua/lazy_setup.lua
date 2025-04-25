11b742c6fe6a2fa740b5d9f2e31317b6d6c5ba8c
JAVV <vinas.johnandrei@gmail.com>
2025-04-25 14:15:01 +0800

    added indent scope plugin

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -89,6 +89,7 @@ lazy.setup({
   -- UI require plugins
   { "nvim-tree/nvim-web-devicons" },
   { "nvim-lua/plenary.nvim" },
+  { "echasnovski/mini.indentscope" },
   { 
     "akinsho/bufferline.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" }
